f00f6956ab9285b7c1fdd35cdda5532dfdeda885
amantyagi22 <amantyagi2k@gmail.com>
2022-07-04 17:57:27 +0530

    First Version

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+console.log(`
+Hi ! My name is Aman Tyagi, 
+I’m a computer Science and Engineering Undergraduate from Ajay Kumar Garg Engineering College.
+I have deep interest in development. I am also a software engineering Intern in Sarv Webs which is in Jaipur. 
+Apart from that I am a Senior student coordinator at Computer Society of India in Our College guiding juniors over development and competitive programming
+`);
